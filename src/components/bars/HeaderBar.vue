29a244e292bbfcad1835193ca1a1ffa1a3d0d4d9
Chase Terry <chase.terry87@gmail.com>
2022-01-09 10:29:18 -0600

    Adjusted responsive header

--- src/components/bars/HeaderBar.vue
+++ src/components/bars/HeaderBar.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex items-center justify-between bg-white px-8 py-4">
     <div>{{ title }}</div>
-    <nav class="flex items-center text-xl md:text-sm gap-4">
+    <nav class="flex items-center text-xl sm:text-sm gap-4">
       <a href="#">
-        <i class="fas fa-bell md:hidden"></i>
-        <span class="hidden md:block"> Notifications </span>
+        <i class="fas fa-bell sm:hidden"></i>
+        <span class="hidden sm:block"> Notifications </span>
       </a>
       <a href="#">
-        <i class="fas fa-cog md:hidden"></i>
-        <span class="hidden md:block"> Settings </span>
+        <i class="fas fa-cog sm:hidden"></i>
+        <span class="hidden sm:block"> Settings </span>
       </a>
       <a href="#" class="md:hidden" @click.prevent="openSidebar">
         <i class="fas fa-bars"></i>
